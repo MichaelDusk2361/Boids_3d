ff4049f20fdd00da2660004ca11c8ca4419b568b
Michael Duskanich <michael.duskanich@gmail.com>
2022-04-24 00:08:29 +0200

    Add cohesion, alignment and separation with performance improvement

--- Boids_3d/Assets/Scripts/Boid.cs
+++ Boids_3d/Assets/Scripts/Boid.cs
@@ -7,17 +7,51 @@ public class Boid : MonoBehaviour
 {
     BoidsController _boidsController;
     [SerializeField] float _speed = 5;
+    [SerializeField] Vector3 _boidRegion;
+    public Vector3 BoidRegion { get => _boidRegion; private set => _boidRegion = value; }
 
     internal void Init(BoidsController boidsController)
     {
         _boidsController = boidsController;
     }
-
+    
     private void Update()
     {
+        _boidRegion = _boidsController.UpdateBoidRegion(this);
         transform.Translate(_speed * Time.deltaTime * Vector3.forward);
-        Debug.Log(Quaternion.LookRotation(_boidsController.targetDirection));
-        Debug.Log(Quaternion.FromToRotation(Vector3.forward, _boidsController.targetDirection));
-        transform.rotation = Quaternion.LookRotation(_boidsController.targetDirection);
+        //transform.rotation = Quaternion.LookRotation(_boidsController.targetDirection);
+        Vector3 separationTarget = Vector3.zero;
+        Vector3 alignmentTarget = Vector3.zero;
+        Vector3 cohesionPosition = Vector3.zero;
+        foreach (var boid in _boidsController.BoidRegions[_boidRegion])
+        {
+            if (boid != this)
+            {
+                alignmentTarget += boid.transform.forward;
+                cohesionPosition += boid.transform.position;
+                separationTarget += (transform.position - boid.transform.position).normalized * 1 / Vector3.Distance(transform.position, boid.transform.position);
+            }
+        }
+        alignmentTarget /= _boidsController.Boids.Count - 1;
+        alignmentTarget = (alignmentTarget - Vector3.Dot(transform.forward, alignmentTarget) * transform.forward).normalized;
+
+        //if (Vector3.Cross(alignmentTarget, transform.forward) != Vector3.zero)
+        //{
+        //    alignmentTarget = Vector3.LerpUnclamped(transform.forward, alignmentTarget, Mathf.Abs(90 / Vector3.Angle(transform.forward, alignmentTarget)));
+        //}
+        //else
+        //    alignmentTarget = Vector3.zero;
+        cohesionPosition /= _boidsController.Boids.Count - 1;
+
+        Vector3 targetVector =
+            ((cohesionPosition - transform.position) * _boidsController.CohesionFactor +
+            separationTarget * _boidsController.SeparationFactor +
+            alignmentTarget * _boidsController.AlignmentFactor);
+        if (targetVector == Vector3.zero)
+            targetVector = new Vector3(transform.forward.x, transform.forward.y, transform.forward.z);
+        transform.rotation = Quaternion.Lerp(
+                                transform.rotation,
+                                Quaternion.LookRotation(targetVector),
+                                Time.deltaTime * _boidsController.TurnSpeed);
     }
 }
--- Boids_3d/Assets/Scripts/BoidsController.cs
+++ Boids_3d/Assets/Scripts/BoidsController.cs
@@ -11,9 +11,19 @@ public class BoidsController : MonoBehaviour
     [SerializeField] int _spawnFieldHeight = 10;
     [SerializeField] int _spawnFieldWidth = 10;
     [SerializeField] int _spawnFieldDepth = 10;
-    public Vector3 targetDirection = Vector3.left;
+    [SerializeField] private float _separationFactor = 1;
+    [SerializeField] private float _alignmentFactor = 1;
+    [SerializeField] private float _cohesionFactor = 1;
+    [SerializeField] private float _turnSpeed = 1;
+    [SerializeField] private float _boidRegionScale = 10;
+    Dictionary<Vector3, List<Boid>> _boidRegions = new();
 
     public List<Boid> Boids { get => _boids; }
+    public float SeparationFactor { get => _separationFactor; }
+    public float AlignmentFactor { get => _alignmentFactor; }
+    public float CohesionFactor { get => _cohesionFactor; }
+    public float TurnSpeed { get => _turnSpeed; }
+    public Dictionary<Vector3, List<Boid>> BoidRegions { get => _boidRegions; set => _boidRegions = value; }
 
     private void OnValidate()
     {
@@ -46,9 +56,39 @@ public class BoidsController : MonoBehaviour
         }
     }
 
+    internal Vector3 GetBoidRegion(Boid boid) =>
+        new(Mathf.Round(boid.transform.position.x * _boidRegionScale),
+            Mathf.Round(boid.transform.position.y * _boidRegionScale),
+            Mathf.Round(boid.transform.position.z * _boidRegionScale));
+
+    internal Vector3 UpdateBoidRegion(Boid boid)
+    {
+        Vector3 boidRegion = GetBoidRegion(boid);
+        if (boidRegion == boid.BoidRegion)
+            return boidRegion;
+
+        if (_boidRegions.ContainsKey(boid.BoidRegion))
+            if (_boidRegions[boid.BoidRegion].Contains(boid))
+            {
+                _boidRegions[boid.BoidRegion].Remove(boid);
+                if (_boidRegions[boid.BoidRegion].Count == 0)
+                    _boidRegions.Remove(boid.BoidRegion);
+            }
+
+        if (!_boidRegions.ContainsKey(boidRegion))
+            _boidRegions.Add(boidRegion, new());
+        _boidRegions[boidRegion].Add(boid);
+        return boidRegion;
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(transform.position, new Vector3(_spawnFieldWidth, _spawnFieldHeight, _spawnFieldDepth));
+        foreach (var item in _boidRegions)
+        {
+            Gizmos.color = new Color(1, 0, 0, .15f);
+            Gizmos.DrawCube(item.Key * 1 / _boidRegionScale, Vector3.one * 1 / _boidRegionScale);
+        }
     }
 }
