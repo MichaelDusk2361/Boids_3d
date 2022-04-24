0f6ab9c38c30612de4339e19d40ca7c5c1002403
Michael Duskanich <michael.duskanich@gmail.com>
2022-04-24 02:34:00 +0200

    Fix unknown key (0,0,0)

--- Boids_3d/Assets/Scripts/Boid.cs
+++ Boids_3d/Assets/Scripts/Boid.cs
@@ -7,7 +7,7 @@ public class Boid : MonoBehaviour
 {
     BoidsController _boidsController;
     [SerializeField] float _speed = 5;
-    [SerializeField] Vector3 _boidRegion = Vector3.negativeInfinity;
+    Vector3 _boidRegion = Vector3.positiveInfinity;
     public Vector3 BoidRegion { get => _boidRegion; private set => _boidRegion = value; }
 
     internal void Init(BoidsController boidsController)
@@ -39,13 +39,12 @@ public class Boid : MonoBehaviour
             alignmentTarget = (alignmentTarget - Vector3.Dot(transform.forward, alignmentTarget) * transform.forward).normalized;
             cohesionPosition /= boidCount - 1;
         }
-
         Vector3 targetVector =
             (cohesionPosition - transform.position) * _boidsController.CohesionFactor +
             separationTarget * _boidsController.SeparationFactor +
             alignmentTarget * _boidsController.AlignmentFactor;
         if (targetVector == Vector3.zero)
-            targetVector = new Vector3(transform.forward.x, transform.forward.y, transform.forward.z);
+            targetVector = Vector3.forward;
         transform.rotation = Quaternion.Lerp(
                                 transform.rotation,
                                 Quaternion.LookRotation(targetVector),
